85641714a63e1e53ddfa9aa1ba597b95fb820be8
lucaxartx <ujude1000@gmail>
2025-03-26 17:19:21 +0100

    feat: add profile routes and support for form data handling

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,18 @@ import connectDB from './config/db';
 import settings from './config/settings';
 import authRoutes from './routes/user.route'
 import { errorHandler } from './middlewares/errorHandler';
-import { log } from 'util';
+import profileRoutes from './routes/profile.route'
 
 connectDB();
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false })) //handle form data like the photos we'll do 
+
 
 app.use('/api/auth', authRoutes);
+app.use('/api/profile', profileRoutes)
 
 
 app.get('/', (req, res) => {
