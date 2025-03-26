5a7c327387dc952112ff53bd6cbc5d216aff2a9d
lucaxartx <ujude1000@gmail>
2025-03-26 17:22:27 +0100

    feat: added cloud configuration settings to the application

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -2,10 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-interface ISettings{
+interface ISettings {
     MONGO_URI: string,
     PORT: string,
     JWT_SECRET_KEY: string,
+    CLOUD_NAME: string
+    CLOUD_API_KEY: string
+    CLOUD_API_SECRET: string
+
 }
 
 
@@ -13,7 +17,12 @@ const settings: ISettings = {
     MONGO_URI: process.env.MONGO_URI as string,
     PORT: process.env.PORT as string,
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY as string,
+    CLOUD_NAME: process.env.CLOUD_NAME as string,
+    CLOUD_API_KEY: process.env.CLOUD_API_KEY as string,
+    CLOUD_API_SECRET: process.env.CLOUD_API_SECRET as string,
+
 }
+
 
 console.log(settings)
 
